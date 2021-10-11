1bd7f29a9998a1898a17c365ebe676c61f3771f9
Manasa <mkotagiri@sidecarhealth.com>
2021-10-11 16:38:09 -0700

    test

---
+++ YouTubeUITests/YouTubeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  YouTubeUITestsLaunchTests.swift
+//  YouTubeUITests
+//
+//  Created by Manasa Kotagiri on 10/11/21.
+//  Copyright © 2021 Haik Aslanyan. All rights reserved.
+//
+
+import XCTest
+
+class YouTubeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
